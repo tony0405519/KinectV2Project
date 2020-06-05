fc9b12362c3551aa5a05e436096a73df40a32957
Pin-ling <56031028+Pin-ling@users.noreply.github.com>
2020-06-05 20:57:24 +0800

    UI
    暫停畫面(從scene改成game上的object)，button功能

---
+++ Assets/Scenes/ButtonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    
+    public void start()
+    {
+        MainMgr.inst.changeScene(SceneID.Game);
+    }
+
+    public GameObject pauseMenu;
+
+    public void pause()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void restart()
+    {
+        MainMgr.inst.changeScene(SceneID.Start);
+    }
+
+    public GameObject soundButton;
+
+    public void soundOff()
+    {
+        soundButton.SetActive(true);
+    }
+
+    public void soundOn()
+    {
+        soundButton.SetActive(false);
+    }
+}
