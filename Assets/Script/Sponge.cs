f783524468f0ee430eafeeb2f8da4ae120b8c598
艾奎華 <qaz620300103@gmail.com>
2020-06-05 16:10:04 +0800

    Sponge.cs Done

--- Assets/Script/Sponge.cs
+++ Assets/Script/Sponge.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 
-public class Sponge : MonoBehaviour {
-    
-    private BoxCollider m_Collider;
-    
-    void Start() {
+[RequireComponent(typeof(MeshCollider), typeof(MeshFilter))]
+public class Sponge : MonoBehaviour
+{
+    private MeshCollider m_meshCollider;
+    private MeshFilter m_meshFilter;
+    private void Awake()
+    {
+        m_meshCollider = GetComponent<MeshCollider>();
+        m_meshFilter= GetComponent<MeshFilter>();
+        m_meshCollider.convex = true;
+    }
+
+    void Start()
+    {
         //m_Collider.isTrigger = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("is Trigger----");
+        Debug.Log(name + " hit " + other.name);
         if (other.tag == "Player")
         {
-            Debug.Log("is Trigger");
+            Debug.Log("Player hit");
             GameMgr.inst.playerHit();
+            Destroy(gameObject);
         }
     }
-
-
-
 }
